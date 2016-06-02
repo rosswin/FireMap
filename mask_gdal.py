f9fbff2e24ef919def8257fa3e83eb0e4b65a472
rosswin <ross.winans@gmail.com>
2016-06-02 08:07:38 -1000

    Fixed align. Added NBR functions.

--- mask_gdal.py
+++ mask_gdal.py
@@ -12,107 +12,153 @@ from osgeo import gdal, gdal_array
 import numpy as np
 
 import sys
-from os import walk 
+from os import walk, makedirs 
 from os.path import join, basename, exists
 #import datetime
 
+def rasterCalc(workedOn, bandsList):
+    sortList = sorted(workedOn)
+    ind = 0
+    while ind < len(sortList) - 1:
+        for band in bandsList:
+            nex = ind + 1
+            print '\ncomparing band %s of: '%band + basename(sortList[ind]) + ' and ' + basename(sortList[nex]) + '\n'
+            
+            band_1_gdal = gdal.Open(join(sortList[ind],basename(sortList[ind]) + '_B%s_mask_align.tif'%band))
+            band_1 = band_1_gdal.GetRasterBand(1)
+            band_1_array = band_1.ReadAsArray()
+            gt1 = band_1_gdal.GetGeoTransform()
+            proj1 = band_1_gdal.GetProjectionRef()
+            
+            
+            band_2_gdal = gdal.Open(join(sortList[nex],basename(sortList[nex]) + '_B%s_mask_align.tif'%band))
+            band_2 = band_2_gdal.GetRasterBand(1)
+            band_2_array = band_2.ReadAsArray()
+            
+            out = join(sortList[ind], str(basename(sortList[ind]))[13:16] + '-' + str(basename(sortList[nex]))[13:16] + '_B%s.tif'%band)
+            
+            array1 = band_1_array > -9999
+            print array1.shape
+            array2 = band_2_array > -9999
+            print array2.shape
+            
+            mask = np.logical_and(array1, array2)
+            
+            maskedData = np.where(mask == True, np.absolute((band_1_array - band_2_array) / band_1_array), -9999)
+        
+            writeGeotiff(out, maskedData, gt1, proj1)
+        ind+=1
+        
+def deltaNBR(workedOn):
+    sortList = sorted(workedOn)
+    ind = 0
+    while ind < len(sortList) - 1:
+        for rstr in sortList:
+            nex = ind + 1
+            print '\nDeltaing NBR1s of: ' + basename(sortList[ind]) + ' and ' + basename(sortList[nex]) + '\n'
+            
+            nbr1 = gdal.Open(join(sortList[ind],basename(sortList[ind]) + '_NBR1.tif'))
+            nbr1Arr = nbr1.ReadAsArray()
+            gt = nbr1.GetGeoTransform()
+            proj = nbr1.GetProjectionRef()
+            
+            nbr2 = gdal.Open(join(sortList[nex],basename(sortList[nex]) + '_NBR1.tif'))
+            nbr2Arr = nbr2.ReadAsArray()
+            
+            filtered1 = nbr1Arr > -9999
+            filtered2 = nbr2Arr > -9999
+            
+            mask = np.logical_and(filtered1, filtered2)
+            
+            maskedData = np.where(mask == True, nbr1Arr - nbr2Arr, -9999)
+            
+            outName = join(sortList[ind], str(basename(sortList[ind]))[13:16] + '-' + str(basename(sortList[nex]))[13:16] + 'dnbr1.tif')
+            
+            writeGeotiff(outName, maskedData, gt, proj)
+        ind+=1
+        
+def nbr(workedOn):
+    for rstr in workedOn:
+        ds5 = gdal.Open(join(rstr, basename(rstr) + '_B5_mask_align.tif'))
+        band5Arr = ds5.ReadAsArray()
+        
+        gt = ds5.GetGeoTransform()
+        proj = ds5.GetProjectionRef()
+        
+        ds6 = gdal.Open(join(rstr, basename(rstr) + '_B6_mask_align.tif'))
+        band6Arr = ds6.ReadAsArray()
+        
+        ds7 = gdal.Open(join(rstr, basename(rstr) + '_B7_mask_align.tif'))
+        band7Arr = ds7.ReadAsArray()
+        
+        filter5 = band5Arr > -9999
+        filter6 = band6Arr > -9999
+        filter7 = band7Arr > -9999
+        mask = np.logical_and(filter5, filter6, filter7)
+        
+        nbr1 = np.where(mask == True, (band5Arr - band7Arr)/(band5Arr + band7Arr), -9999)
+        
+        nbr2 = np.where(mask == True, (band6Arr - band7Arr)/(band6Arr + band7Arr), -9999)
+        
+        out1 = join(rstr, basename(rstr) + '_NBR1.tif')
+        out2 = join(rstr, basename(rstr) + '_NBR2.tif')
+        
+        writeGeotiff(out1, nbr1, gt, proj)
+        writeGeotiff(out2, nbr2, gt, proj)
+
 def alignMasks(workedOn,bandsList):
     #code adapted from http://sciience.tumblr.com/post/101722591382/finding-the-georeferenced-intersection-between-two
-    # load data
+
+    # prep some lists we will need
     sortList = sorted(workedOn)
-    anchorDs = gdal.Open(join(sortList[0], basename(sortList[0])+'_B%s.tif'%bandsList[0]))
-    anchorBand = anchorDs.GetRasterBand(1)
-    anchorGt = anchorDs.GetGeoTransform()
-    anchorProj = anchorDs.GetProjectionRef()
+    ltrbs = []
+    
+    #scan all of our input rasters and grab their geotransform, convert that to a bounding box
     for rstr in sortList:
         for bnd in bandsList:
-            ds = gdal.Open(join(rstr, basename(rstr)+'_B%s_mask.tif'%bnd))
+            ds = gdal.Open(join(rstr, basename(rstr) + '_B%s_mask.tif'%bnd), gdal.GA_ReadOnly)
             band = ds.GetRasterBand(1)
             gt = ds.GetGeoTransform()
-            proj = ds.GetProjectionRef()
-            # r1 has left, top, right, bottom of dataset's bounds in geospatial coordinates.
-            anchorR = [anchorGt[0], anchorGt[3], anchorGt[0] + (anchorGt[1] * anchorDs.RasterXSize), anchorGt[3] + (anchorGt[5] * anchorDs.RasterYSize)]
-            r = [gt[0], gt[3], gt[0] + (gt[1] * ds.RasterXSize), gt[3] + (gt[5] * ds.RasterYSize)]
-            print '\t1 bounding box: %s' % str(anchorR)
-            print '\t2 bounding box: %s' % str(r)
+            ltrb = [gt[0], gt[3], gt[0] + (gt[1] * ds.RasterXSize), gt[3] + (gt[5] * ds.RasterYSize), gt[1], gt[5], ds.RasterXSize, ds.RasterYSize]
+            ltrbs.append(ltrb)
+            ds = None
             
-            # find intersection between bounding boxes
-            intersection = [max(anchorR[0], r[0]), min(anchorR[1], r[1]), min(anchorR[2], r[2]), max(anchorR[3], r[3])]
-            if anchorR != r:
-                print '\t** different bounding boxes **'
-                # check for any overlap at all...
-                if (intersection[2] < intersection[0]) or (intersection[1] < intersection[3]):
-                    intersection = None
-                    print '\t***no overlap***'
-                    return
-                else:
-                    print '\tintersection:',intersection
-                    left1 = int(round((intersection[0]-anchorR[0])/anchorGt[1])) # difference divided by pixel dimension
-                    top1 = int(round((intersection[1]-anchorR[1])/anchorGt[5]))
-                    col1 = int(round((intersection[2]-anchorR[0])/anchorGt[1])) - left1 # difference minus offset left
-                    row1 = int(round((intersection[3]-anchorR[1])/anchorGt[5])) - top1
-                    
-                    left2 = int(round((intersection[0]-r[0])/gt[1])) # difference divided by pixel dimension
-                    top2 = int(round((intersection[1]-r[1])/gt[5]))
-                    col2 = int(round((intersection[2]-r[0])/gt[1])) - left2 # difference minus new left offset
-                    row2 = int(round((intersection[3]-r[1])/gt[5])) - top2
-                    
-                    #print '\tcol1:',col1,'row1:',row1,'col2:',col2,'row2:',row2
-                    if col1 != col2 or row1 != row2:
-                        print "*** MEGA ERROR *** COLS and ROWS DO NOT MATCH ***"
-                    # these arrays should now have the same spatial geometry though NaNs may differ
-                    anchorArray = anchorBand.ReadAsArray()
-                    #bandArray = band.ReadAsArray(left2,top2,col2,row2)
-                    bandArray = band.ReadAsArray(int(anchorR[0]),int(anchorR[1]),int(anchorR[2]),int(anchorR[3]))
-                    
-            else: # same dimensions from the get go
-                col1 = anchorDs.RasterXSize # = col2
-                row1 = anchorDs.RasterYSize # = row2
-                anchorArray = anchorBand.ReadAsArray()
-                bandArray = band.ReadAsArray()
-                
-            #Time to test if the aligned arrays are the same size.
-            fillVal = -9999
-            if anchorArray.shape[0] > bandArray.shape[0]:
-                print 'array1 has more rows... Padding array2 with -9999.'
-                pad = anchorArray.shape[0] - bandArray.shape[0]
-                temp = np.ones((pad, anchorArray.shape[1])) * fillVal
-                print 'temp shape: ' + str(temp.shape)
-                bandArray = np.append(bandArray, temp, axis=0)
-            elif anchorArray.shape[0] < bandArray.shape[0]:
-                print 'array2 has more rows... Padding array1 with -9999.'
-                pad = bandArray.shape[0] - anchorArray.shape[0]
-                temp = np.ones((pad, bandArray.shape[1])) * fillVal
-                anchorArray = np.append(anchorArray, temp, axis=0)
-            elif anchorArray.shape[1] > bandArray.shape[1]:
-                #this one works!!!
-                print 'array1 has more cols... Padding array2 with -9999'
-                pad = anchorArray.shape[1] - bandArray.shape[1]
-                temp = np.ones((anchorArray.shape[0], pad)) * fillVal
-                bandArray = np.append(bandArray, temp, axis=1)
-            elif anchorArray.shape[1] < bandArray.shape[1]:
-                print 'array2 has more cols... Padding array1 with -9999.'
-                pad = bandArray.shape[1] - anchorArray.shape[1]
-                temp = np.ones((bandArray.shape[0], pad)) * fillVal
-                anchorArray = np.append(anchorArray, temp, axis=1)
-            else:
-                print 'arrays are equal. No padding required.'
-                
+    #intitalize the intersections with some plausible values (the first raster)
+    intersect = [ltrbs[0][0],ltrbs[0][1],ltrbs[0][2],ltrbs[0][3]]
+    
+    #update our intersects with all of the geotransforms we collected earlier
+    for ltrb in ltrbs:
+        if ltrb[0] > intersect[0]:
+            intersect[0] = ltrb[0]
+        if ltrb[3] > intersect[3]:
+            intersect[3] = ltrb[3]
+        if ltrb[1] < intersect[1]:
+            intersect[1] = ltrb[1]
+        if ltrb[2] < intersect[2]:
+            intersect[2] = ltrb[2]
+    
+    #time to come up with an offset for each raster based on intersect. We will clip to the intersection bounding box.
+    for rstr in sortList:
+        print str(rstr)
+        for bnd in bandsList:
+            ds = gdal.Open(join(rstr, basename(rstr) + '_B%s_mask.tif'%bnd), gdal.GA_ReadOnly)
+            band = ds.GetRasterBand(1)
+            gt = ds.GetGeoTransform()
+            proj = ds.GetProjection()
+            offsetX = int(round((intersect[0]-gt[0])/gt[1])) # difference divided by pixel dimension
+            offsetY = int(round((intersect[1]-gt[3])/gt[5]))
+            col = int(round((intersect[2]-gt[0])/gt[1])) - offsetX # difference minus offset left
+            row = int(round((intersect[3]-gt[3])/gt[5])) - offsetY
+        
+            bandArray = band.ReadAsArray(offsetX, offsetY, col, row)
+            intersectGt = (intersect[0], gt[1], 0, intersect[1], 0, gt[5])
             outName = join(rstr, basename(rstr) + '_B%s_mask_align.tif'%bnd)
             
-            print 'Testing array sizes...'
-            print 'array1: ' + str(anchorArray.shape)
-            print 'array2: ' + str(bandArray.shape)
-            if anchorArray.shape != bandArray.shape:
-                print 'im not going to WRITE ' + outName + ' because arrays still dont match. tell ross to fix the janky array size comparisson code block.'
-            else:
-                writeGeotiff(outName, bandArray, anchorGt, anchorProj)
-
-
+            writeGeotiff(outName, bandArray, intersectGt, proj)
 
 def writeGeotiff(fname, data, geo_transform, projection):
     """Create a GeoTIFF file with the given data."""
-    print 'write_geotiff writing ' + fname +'\n'
+    print '\nwrite_geotiff writing ' + fname +'\n'
 
     driver = gdal.GetDriverByName('GTiff')
     rows, cols = data.shape
@@ -159,8 +205,10 @@ def maskClouds(inPath, bd, maskArr):
         
         
         maskedData = np.where((maskArr == 0), data, -9999).astype(bitType)
+        outName = join(inPath, basename(inPath)+'_B%s_mask.tif'%bd)
         
-        return(maskedData, geo_transform, proj)
+        writeGeotiff(outName, maskedData, geo_transform, proj)
+        
             
     else:
         print 'maskArray could not find:\n ' + curBand + '. This program requires TOARs to exist in Landsat folder. Exiting.\n'
@@ -198,26 +246,38 @@ def cloudArray(inPath):
     else:
         print 'cloudArray could not find:\n ' + mtlf + '. This program requires that FMASK with default name already exist in the Landsat folders. Exiting.\n'
         sys.exit(1)
-        
-        
-inputDir = r'F:\Guam_Fire_Mapping\Imagery\Landsat'
-bandsList = [4,5,6,7]
+
+
+inputDir = r'G:\Guam_Fire_Mapping\Imagery\Landsat'
+#outDir = r'G:\Guam_Fire_Mapping\firePrep'
+bandsList = [5,6,7]
 workedOn = []
+
+'''
+#make a prep folder to hold outputs:
+if exists(outDir):
+    print 'outputs to: ' + outDir
+else:
+    print 'making ' + outDir
+    makedirs(outDir)
+'''    
+
+#start the main program
 for root, dirs, files in walk(inputDir):
     for di in dirs:
         curPath = join(root, di)
-        print 'MAIN working on:\n' + curPath + '\n'
+        print 'MAIN:\n' + curPath + '\n'
+        #Prep one at a time. But lets keep a running tally of all the directories we have worked on. After prep we are going need to compare all images at once.
         workedOn.append(curPath)
-        
+        '''
+        #grab the current cloudarray, make a NaN array
         cloudArr = cloudArray(curPath)
         for band in bandsList:
-            maskLS, geo_transform, proj = maskClouds(curPath, band, cloudArr)
-            outmask = join(curPath, basename(curPath)+'_B%s_mask.tif'%band)
-            writeGeotiff(outmask, maskLS, geo_transform, proj)
-        
-        alignMasks(workedOn, bandsList)
-        
-            
+            maskClouds(curPath, band, cloudArr)
+        '''
+#alignMasks(workedOn, bandsList)
+nbr(workedOn)
+deltaNBR(workedOn)
 
 
         
